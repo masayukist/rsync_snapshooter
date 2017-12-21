193d6df255919702291c225cb781711b6c4b8298
root <root@macpro.cal.is.tohoku.ac.jp>
2017-12-21 17:33:09 +0900

    create lockfile in /tmp

--- create.sh
+++ create.sh
@@ -5,7 +5,10 @@ export BACKUP_ROOT=$(cd $(dirname ${0}) && pwd)
 source ${BACKUP_ROOT}/config.sh
 source ${BACKUP_ROOT}/include/path.sh
 
+DIRSTR=`echo ${BACKUP_ROOT} | sed 's/\//\_/g'`
+LOCK_FILE=/tmp/${DIRSTR}_lock
+
 (
     flock -s 200
     ${BACKUP_ROOT}/include/snapshot.sh
-) 200>${SNAPSHOTS_DIR}/lock
+) 200>${LOCK_FILE}
